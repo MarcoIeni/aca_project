9eb9ca331d1c7a5d78be0c007927866115c035de
MarcoIeni <marcoieni94@gmail.com>
2017-06-30 14:30:32 +0200

    added presentation

---
+++ presentation/scripts/terminal.language.js
@@ -0,0 +1,28 @@
+/*
+  Language: terminal console
+  Author: Josh Bode <joshbode@gmail.com>
+*/
+
+hljs.registerLanguage('terminal', function() {
+  return {
+    contains: [
+      {
+        className: 'string',
+        begin: '^([\\w.]+)@([\\w.]+)'
+      },
+      {
+        className: 'string',
+        begin: '>>'
+      },
+      {
+        className: 'constant',
+        begin: ' (.*) \\$ '
+      },
+      {
+        className: 'ansi',
+        begin: '<span style\\="([^"]+)">',
+        end: '<\\/span>'
+      }
+    ]
+  }
+});
